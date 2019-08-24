8db0841fa62c4e3268491ae4026a6e158d549d0d
Aiswarya Nair <aishcv7498@gmail.com>
2019-08-24 12:44:23 +0530

    centre

--- Classwork.py
+++ Classwork.py
@@ -2,3 +2,9 @@ class Basic_stats(object):
 
     def __init__(self,value):
         self.value=value
+
+    import pandas as pd
+
+def median(self):
+    a = df.median(axis = 1, skipna = True) 
+    return a
